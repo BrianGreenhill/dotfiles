79b04032c2e1eb1645c5b771d92e90a9e499109f
Brian Greenhill <bg@hellofresh.com>
2019-05-04 13:54:18 +0200

    volume script

--- volume.sh
+++ volume.sh
@@ -7,10 +7,9 @@
  mute="$(amixer -c 1 -D pulse get Master | tail -1 | awk '{print $6}' | sed 's/[^a-z]*//g')"
  if [[ $volume == 0 || "$mute" == "off" ]]; then
      pactl set-sink-mute @DEFAULT_SINK@ toggle > /dev/null
-     dunstify -a "changeVolume" -u low -i multimedia-volume-control-symbolic -r "$msgId" "Volume muted" 
+     dunstify -a "changeVolume" -u low -r "$msgId" "Volume muted"
  else
-     dunstify -a "changeVolume" -u low -i multimedia-volume-control-symbolic -r "$msgId" \
-     "Volume: ${volume}%"
+     dunstify -a "changeVolume" -u low -r "$msgId" "Volume: ${volume}%"
  fi
  canberra-gtk-play -i audio-volume-change -d "changeVolume"
 
