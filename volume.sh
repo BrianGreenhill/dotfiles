ab6ea7f950a203a4b9c6489afb9c96686ff24cef
Brian Greenhill <brian.greenhill@ecosia.org>
2019-11-29 11:39:45 +0100

    updates volume script and gitconfig

--- volume.sh
+++ volume.sh
@@ -1,15 +1,15 @@
- #!/bin/bash
- # changeVolume
+#!/bin/bash
+# changeVolume
 
- msgId="991049"
- pactl set-sink-volume @DEFAULT_SINK@ "$@" > /dev/null
- volume="$(amixer -c 1 -D pulse get Master | tail -1 | awk '{print $5}' | sed 's/[^0-9]*//g')"
- mute="$(amixer -c 1 -D pulse get Master | tail -1 | awk '{print $6}' | sed 's/[^a-z]*//g')"
- if [[ $volume == 0 || "$mute" == "off" ]]; then
-     pactl set-sink-mute @DEFAULT_SINK@ toggle > /dev/null
-     dunstify -a "changeVolume" -u low -r "$msgId" "Volume muted"
- else
-     dunstify -a "changeVolume" -u low -r "$msgId" "Volume: ${volume}%"
- fi
- canberra-gtk-play -i audio-volume-change -d "changeVolume"
+msgId="991049"
+pactl set-sink-volume @DEFAULT_SINK@ "$@" > /dev/null
+volume="$(amixer -D pulse | grep -A 5 Master | tail -1 | awk '{print $5}' | sed 's/[^0-9]*//g')"
+mute="$(amixer -c 1 -D pulse get Master | tail -1 | awk '{print $6}' | sed 's/[^a-z]*//g')"
+
+if [[ $volume == 0 || "$mute" == "off" ]]; then
+  pactl set-sink-mute @DEFAULT_SINK@ toggle > /dev/null
+  dunstify -a "changeVolume" -u low -r "$msgId" "Volume muted"
+else
+  dunstify -a "changeVolume" -u low -r "$msgId" "Volume: ${volume}%"
+fi
 
