aa87e4fd8d63bee060abf28f8421433f1a6d3918
Brian Greenhill <briangreenhill@github.com>
2024-07-10 08:37:47 +0200

    another dotfiles commit...

--- install.sh
+++ install.sh
@@ -8,8 +8,9 @@ directories=$(find . -maxdepth 1 -type d -not -name ".*" | sed 's|./||')
 
 # check if CODESPACES env var is true
 if [[ $CODESPACES == "true" ]]; then
-    echo "installing package dependencies"
+    echo "installing fzf"
     sudo apt-get -y -q install fzf
+    echo "pulling nvim config"
     pushd /workspaces/.codespaces/.persistedshare/dotfiles
     git submodule update --init --recursive
     echo "creating symlinks..."
@@ -18,9 +19,12 @@ if [[ $CODESPACES == "true" ]]; then
         echo "symlinked $dir..."
         ln -sf /workspaces/.codespaces/.persistedshare/dotfiles/$dir/.config/$dir ~/.config/$dir
     done
+    echo "==> install vim plugins"
+    nvim --headless +PlugInstall +qall
     echo "==> installing ruby"
     rbenv install 3.3.1 -s
     rbenv global 3.3.1
+    sudo apt install python3.8-venv -y -q
     echo "done"
     popd
     exit 0
