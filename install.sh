77c250cffa5833c8fddca798c0342958f9c13536
BrianGreenhill <briangreenhill@github.com>
2024-08-15 19:25:20 +0200

    [automated] sync from commit.sh

--- install.sh
+++ install.sh
@@ -1,50 +1,31 @@
 #!/usr/bin/env bash
 
+# This script is intended to be run in a codespace
+
 set -e
 
-# get directories in the current directory
-# exclude directories that start with a dot
-directories=$(find . -maxdepth 1 -type d -not -name ".*" | sed 's|./||')
-
-function installfzf() {
+function installfzf {
     if [[ -z $CODESPACES  ]]; then echo "CODESPACES env var is not set"; exit 1; fi
-    echo "==> installing latest fzf..."
+    echo "installing latest fzf..."
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
     ~/.fzf/install --all
     echo 'source ~/.fzf.bash' >>~/.bashrc
     echo "done"
 }
 
+function confignvim {
+    echo "configuring nvim..."
+    gh repo clone briangreenhill/config.nvim ~/.config/nvim
+    pushd ~/.config/nvim
+    nvim --headless +PlugInstall +qall
+    popd
+}
+
 # check if CODESPACES env var is true
 if [[ $CODESPACES == "true" ]]; then
-    echo "pulling nvim config"
-    pushd /workspaces/.codespaces/.persistedshare/dotfiles
-    git submodule update --init --recursive
-    echo "creating symlinks..."
-    directories=("tmux" "nvim")
-    for dir in "${directories[@]}"; do
-        echo "symlinked $dir..."
-        ln -sf /workspaces/.codespaces/.persistedshare/dotfiles/"$dir"/.config/"$dir" ~/.config/"$dir"
-    done
-    echo "==> nvim plugins"
-    nvim --headless +PlugInstall +qall
     installfzf
-    echo "==> installing ruby"
-    rbenv install 3.3.1 -s
-    rbenv global 3.3.1
-    sudo apt-get install python3.8-venv -y -q
-    echo "done"
-    popd
+    confignvim
+    echo "configuring tmux"
+    ln -sf /workspaces/.codespaces/.persistedshare/dotfiles/tmux/.config/tmux ~/.config/tmux
     exit 0
 fi
-
-if [[ $(which stow) == "" ]]; then
-    echo "stow is not installed"
-    exit 1
-fi
-
-# ensure this runs from the user home directory
-for dir in "${directories[@]}"; do
-    echo "stowing $dir..."
-    stow "$dir"
-done
