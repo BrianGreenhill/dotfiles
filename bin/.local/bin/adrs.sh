2131ab3b77ef3d911297124ff23f8719a3377809
BrianGreenhill <briangreenhill@github.com>
2024-08-17 19:44:35 +0200

    [automated] sync from commit.sh

--- bin/.local/bin/adrs.sh
+++ bin/.local/bin/adrs.sh
@@ -3,28 +3,19 @@
 set -e
 
 function help() {
-    echo '=========================================================='
-    echo '         ______                                         '
-    echo '  __________  /________      ________ _________ _______ '
-    echo '  __  ___/_  /_  __ \_ | /| / /_  __ `__ \  __ `/_  __ \'
-    echo '  _(__  )_  / / /_/ /_ |/ |/ /_  / / / / / /_/ /_  / / /'
-    echo '  /____/ /_/  \____/____/|__/ /_/ /_/ /_/\__,_/ /_/ /_/'
+    echo ' slowman - a slow but powerful pokemon'
     echo ""
     echo "  [adrs.sh] - search adrs"
     echo ""
     echo " Usage:"
     echo "  adrs.sh owner/repo <search term>"
     echo ""
-    echo '========================================================='
     exit 1
 }
+if [[ -z "$1" ]]; then help; fi
 
 # Define repository owner and name
 rwo=${1}
 defaultpath="${2-docs/adrs}"
-
-if [[ -z "$1" ]]; then help; fi
-
 markdown=$(gh api /repos/"$rwo"/contents/"$defaultpath" | jq -r '.[].path' | fzf | xargs -I "{}" gh api /repos/"$rwo"/contents/{} | jq -r '.content' | base64 -d)
-
-echo "$markdown" | bat -l markdown
+echo "$markdown" | glow
